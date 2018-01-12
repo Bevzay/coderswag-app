b7530ed61c3493a868615801004eb457376e8e78
Beverly <beverly@Neils-MacBook-Pro.local>
2018-01-12 16:39:27 +1300

    Collection Views (grid layouts)

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Beverly on 12/01/18.
+//  Copyright © 2018 Beverly. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
